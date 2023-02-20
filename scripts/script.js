5fc8b62e82b41f8ee6b91499f1d06a280bc3187b
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2023-02-20 19:23:31 -0300

    closes #5 javascript adicionado

--- scripts/script.js
+++ scripts/script.js
@@ -2,46 +2,40 @@ $('#inputPhone').mask('(00)00000-0000');
 
 var students = [];
 
-function saveStudent(){
-    
+function saveStudent() {
+  var newStudent = {
+    id: students.length + 1,
+    name: document.getElementById("inputName").value,
+    email: document.getElementById("inputEmail").value,
+    phone: document.getElementById("inputPhone").value,
+    course: document.getElementById("inputCourse").value,
+    turn: document.querySelector('input[name="RadioDefault"]:checked').value,
+  };
 
-    var newStudent = {
-        id: students.length + 1,
-        name: document.getElementById("inputName").value,
-        email: document.getElementById("inputEmail").value,
-        phone: document.getElementById("inputPhone").value,
-        course: document.getElementById("inputCourse").value,
-        turn: document.getElementsByName("RadioDefault").value
-    };
-
-    
-    students.push(newStudent);
-    newRow(newStudent);
+  students.push(newStudent);
+  newRow(newStudent);
 }
 
 
-function newRow(students){
+function newRow(students) {
+  var table = document.getElementById("table");
+  let newRow = table.insertRow();
 
-    var table = document.getElementById("table");
-    let newRow = table.insertRow();
+  var idNode = document.createTextNode(students.id);
+  newRow.insertCell().appendChild(idNode);
 
+  var nameNode = document.createTextNode(students.name);
+  newRow.insertCell().appendChild(nameNode);
 
-    var idNode = document.createTextNode(students.id);
-    newRow.insertCell().appendChild(idNode);
+  var emailNode = document.createTextNode(students.email);
+  newRow.insertCell().appendChild(emailNode);
 
-    var nameNode = document.createTextNode(students.name);
-    newRow.insertCell().appendChild(nameNode);
-    
-    var emailNode = document.createTextNode(students.email);
-    newRow.insertCell().appendChild(emailNode);
+  var phoneNode = document.createTextNode(students.phone);
+  newRow.insertCell().appendChild(phoneNode);
 
-    var phoneNode = document.createTextNode(students.phone);
-    newRow.insertCell().appendChild(phoneNode);
+  var courseNode = document.createTextNode(students.course);
+  newRow.insertCell().appendChild(courseNode);
 
-    var courseNode = document.createTextNode(students.course);
-    newRow.insertCell().appendChild(courseNode);
-
-    var turnNode = document.createTextNode(students.turn);
-    newRow.insertCell().appendChild(turnNode);
-
+  var turnNode = document.createTextNode(students.turn);
+  newRow.insertCell().appendChild(turnNode);
 }
