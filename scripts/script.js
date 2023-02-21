9655e2bb6e7072eddb0b8ee4cfb52daced17e6e1
Marcelo Mendes <99540656+MarceloMendesFatec@users.noreply.github.com>
2023-02-20 21:03:48 -0300

    closes #6 - refatorar codigo

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-$('#inputPhone').mask('(00)00000-0000');
+$('#inputPhone').mask('(00)00000-0000');//mascara para telefone
 
-var students = [];
+var students = []; // array vazio para guardar os objetos de novos estudantes
 
 
 function saveStudent() {
@@ -11,39 +11,37 @@ function saveStudent() {
     phone: document.getElementById("inputPhone").value,
     course: document.getElementById("inputCourse").value,
     turn: document.querySelector('input[name="RadioDefault"]:checked').value,
-  };
+  };  // objeto que recebe um novo aluno
 
-  students.push(newStudent);
-  newRow(newStudent);
+  students.push(newStudent); // acrescenta o novo objeto ao array 
+  newRow(newStudent); // adiciona uma nova linha na tabela com o objeto 
 }
 
 
 function newRow(students) {
-  var table = document.getElementById("table");
-  let newRow = table.insertRow();
+  var table = document.getElementById("table"); // selecionar a tabela
+  let newRow = table.insertRow(); // inserir nova linha na tabela selecionada
 
   var idNode = document.createTextNode(students.id);
-  newRow.insertCell().appendChild(idNode);
+  newRow.insertCell().appendChild(idNode);// inserir o ID na tabela 
 
   var nameNode = document.createTextNode(students.name);
-  newRow.insertCell().appendChild(nameNode);
+  newRow.insertCell().appendChild(nameNode);// inserir o nome na tabela 
 
   var emailNode = document.createTextNode(students.email);
-  var cell = newRow.insertCell();
+  var cell = newRow.insertCell();// inserir o email na tabela com a condicao de esconder a celula quando menor que MD
   cell.className = "d-none d-md-table-cell";
   cell.appendChild(emailNode);
   
 
   var phoneNode = document.createTextNode(students.phone);
-  var cell2 = newRow.insertCell();
+  var cell2 = newRow.insertCell();// inserir o telefone na tabela  com a condicao de esconder a celula quando menor que MD
   cell2.className = "d-none d-md-table-cell";
   cell2.appendChild(phoneNode);
 
   var courseNode = document.createTextNode(students.course);
-  newRow.insertCell().appendChild(courseNode);
+  newRow.insertCell().appendChild(courseNode);// inserir o curso na tabela 
 
   var turnNode = document.createTextNode(students.turn);
-  newRow.insertCell().appendChild(turnNode);
-
- 
+  newRow.insertCell().appendChild(turnNode);// inserir o turno na tabela 
 };
